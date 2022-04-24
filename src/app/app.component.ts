c025700ddd12914df4a1514ccca860987afe5d1d
guntharohith <rohithshetty267@gmail.com>
2022-04-24 11:27:02 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-notification-center';
+
+  callbackFunction(message: any) {
+    alert(message.title)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppTestModule } from 'raven-notifications';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppTestModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
