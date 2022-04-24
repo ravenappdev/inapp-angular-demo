241282247f40a35a67e7847ac64b4556cd277bde
guntharohith <rohithshetty267@gmail.com>
2022-04-24 22:49:49 +0530

    ui changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppTestModule } from 'raven-notifications';
-
+import { RavenInAppModule } from 'raven-in-app-angular';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppTestModule,
+    RavenInAppModule
   ],
   providers: [],
   bootstrap: [AppComponent]
