79ba77f4d5011c84402e96e9722228c58324ffe9
guntharohith <rohithshetty267@gmail.com>
2022-04-25 00:49:39 +0530

    changed library name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RavenInAppModule } from 'raven-in-app-angular';
+import { RavenInAppModule } from 'raven-inapp-angular';
 @NgModule({
   declarations: [
     AppComponent
