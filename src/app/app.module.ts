73e48a0caa6cb0eea101b2cf5709d51003f4943c
priyansh srivastava <priyansh@ravenapp.dev>
2022-04-26 14:24:21 +0530

    updated sdk version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RavenInAppModule } from 'raven-inapp';
+import { RavenInAppModule } from '@ravenapp/raven-inapp-angular';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    RavenInAppModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RavenInAppModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
