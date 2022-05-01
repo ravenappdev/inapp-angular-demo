900f8dbfd7722d3abfaa8c5fea9547f6a6fcdad7
guntharohith <rohithshetty267@gmail.com>
2022-05-01 20:08:19 +0530

    seperated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  userId: any ;
-  appId: any;
-  signature: any;
+  userId: any = 'priyansh';
+  appId: any = 'ead40fc4-34a2-4e7c-abaf-337c00eef79a';
+  signature: any =
+    'a5d61c6d6855a168665b9d69bb0f50318f56c6d5e22879ab8f761d7012ea9d0d';
   title = 'demo-notification-center';
 
   callbackFunction(message: any) {
-    alert(message.title)
+    alert(message.title);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { RavenInAppModule } from '@ravenapp/raven-inapp-angular'
-=======
-import { RavenInAppModule } from '@ravenapp/raven-inapp-angular';
->>>>>>> 83a7b8057c49094b15f44ed7635bb61cfe04f257
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, RavenInAppModule],
