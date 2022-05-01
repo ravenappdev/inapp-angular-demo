a63e70902310f19ade43e7bc19a0ce280a47a767
GUNTHA ROHITH <48819698+guntharohith@users.noreply.github.com>
2022-05-01 22:43:42 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  userId: any = 'priyansh';
-  appId: any = 'ead40fc4-34a2-4e7c-abaf-337c00eef79a';
-  signature: any =
-    'a5d61c6d6855a168665b9d69bb0f50318f56c6d5e22879ab8f761d7012ea9d0d';
+  userId: any ;
+  appId: any ;
+  signature: any ;
   title = 'demo-notification-center';
 
   callbackFunction(message: any) {
